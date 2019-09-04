084ef243977173f7cc384411ba3554dbdff6db90
Buzzactaxe <valiantsimpson@gmail.com>
2019-09-04 08:54:16 +0200

    Add Review Delete function

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -20,9 +20,16 @@ module.exports = {
 	// Reviews Update
 	async reviewUpdate(req, res, next) {
 		await Review.findByIdAndUpdate(req.params.review_id, req.body.review);
-		req.session.success = 'Review update successful';
+		req.session.success = 'Review update successful!';
 		res.redirect(`/posts/${req.params.id}`);
 	},
 	// Reviews Destroy
-	async reviewDestroy(req, res, next) {}
+	async reviewDestroy(req, res, next) {
+		await Post.findByIdAndUpdate(req.params.id, {
+			$pull : { reviews: req.params.review_id }
+		});
+		await Review.findByIdAndRemove(req.params.review_id);
+		req.session.success = 'Review Removed successfuly!';
+		res.redirect(`/posts/${req.params.id}`);
+	}
 };
